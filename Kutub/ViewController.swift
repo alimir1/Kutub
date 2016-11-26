dced1c4461f2726996a458b607f7b6899061dbdd
Ali Mir <110abidi@gmail.com>
2016-11-25 16:43:55 -0800

    got the pods up and working!

--- Kutub/ViewController.swift
+++ Kutub/ViewController.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
+import FolioReaderKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let config = FolioReaderConfig()
+        let bookPath = Bundle.main.path(forResource: "AshuraMisconceptions", ofType: "epub")
+        let epubVC = FolioReaderContainer(withConfig: config, epubPath: bookPath!, removeEpub: false)
+        
+        // Present the epubVC view controller like every other UIViewController instance
+        present(epubVC, animated: true, completion: nil)
     }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Kutub' do
+    pod 'FolioReaderKit', '~> 1.0.0'
+end
